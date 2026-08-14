b56b6f377fe24c8400b8078b50c37065b90d14a1
Jaya Lakshmi <jayalakshmi@gmail.com>
2026-08-14 15:10:35 +0530

    updated color changer

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-let red=Math.floor(Math.random()*255);
-let green=Math.floor(Math.random()*255);
-let blue=Math.floor(Math.random()*255);
-
-let color=`rgb(${red},${green},${blue})`;
-let div=document.querySelector("#colorbox");
-div.style.backgroundColor=color;
 let para=document.querySelector("p");
-para.innerText=color;
+let div=document.querySelector("#colorbox");
+let btn=document.querySelector("button");
+btn.addEventListener("click",()=>{
+    let red=Math.floor(Math.random()*255);
+    let green=Math.floor(Math.random()*255);
+    let blue=Math.floor(Math.random()*255);
 
+    let color=`rgb(${red},${green},${blue})`;
+    div.style.backgroundColor=color;
+    para.innerText=color;
+})
+    
